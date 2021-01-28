09840b5a2814655ad48334f8921f88a682fe5c09
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-28 13:21:08 -0800

    adds screenshot of noteful and saves to folder

--- pingWebsite.js
+++ pingWebsite.js
@@ -2,9 +2,20 @@ const puppeteer = require('puppeteer');
 const { printTime } = require('./printTime');
 
 (async () => {
+  // const browser = await puppeteer.launch();
+  // const page = await browser.newPage();
+  // await page.goto('https://serene-savannah-58732.herokuapp.com/');
+  // await page.screenshot({ path: `${printTime()}.png` });
+  // await browser.close();
+  await getScreenshot('https://serene-savannah-58732.herokuapp.com/');
+  await getScreenshot('https://noteful-deployment.herokuapp.com/');
+})();
+
+async function getScreenshot(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://serene-savannah-58732.herokuapp.com/');
-  await page.screenshot({ path: `${printTime()}.png` });
+  await page.goto(url);
+  let title = url.slice(8, url.indexOf('.herokuapp'))
+  await page.screenshot({ path: `/screenshots/${title}-${printTime()}.png` });
   await browser.close();
-})();
+}
