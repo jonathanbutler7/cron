4d489a8935511030a24f03536fd1c19643a2d319
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-29 11:47:30 -0800

    changes pingWebsite to pingWebsites

--- cron.js
+++ cron.js
@@ -5,7 +5,7 @@ function runCron() {
   cron.schedule('*/20 * * * *', () => {
     // each asterisk is minutes, hours, day of month, month, day of week
     console.log('Scheduler running...');
-    if (shell.exec('node pingWebsite.js').code !== 0) {
+    if (shell.exec('node pingWebsites.js').code !== 0) {
       console.log('something went wrong...');
     }
   });
